87876941bf30959ad2641f18948aaeeabf4be328
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-15 23:06:41 +0530

    loops

---
+++ Loops/while Loop/sumNnumbers.java
@@ -0,0 +1,13 @@
+public class sumNnumbers {
+  public static void main(String[] args) {
+  int sum=0;
+  int n=5;
+  int i=1;
+  while(i<=n){
+    sum+=i;
+    i++;
+  }
+  System.out.println(sum);
+  }
+  
+}
