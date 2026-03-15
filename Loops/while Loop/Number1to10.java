45e4397ab981705adb8ed51f559c93c481cce998
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-15 22:52:44 +0530

    java loop

---
+++ Loops/while Loop/Number1to10.java
@@ -0,0 +1,12 @@
+// printing number 1 to 10
+
+public class Number1to10 {
+  public static void main(String[] args) {
+    int i=0;
+    while(i<=10){
+      System.out.println(i);
+      i++;
+    }
+  }
+
+}
---
+++ Loops/while Loop/whileloopSyntax.java
@@ -0,0 +1,13 @@
+// while loop syantax
+
+public class whileloopSyntax {
+  public static void main(String[] args) {
+    int i=0;
+    while(i<10){
+      System.out.println("Hello");
+      i++;
+    }
+  }
+}
+
+
