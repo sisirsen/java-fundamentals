54b099042e64f20f6a909987a9e9cd6bcebcfd12
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-17 22:29:59 +0530

    functions

---
+++ Functions/declaredFunctions.java
@@ -0,0 +1,12 @@
+public class declaredFunctions {
+  
+  public static int print(){
+    int a =1;
+    int b =1;
+    return a+b;
+
+  }
+  public static void main(String[] args) {
+    System.out.print(print());
+  }
+}
---
+++ Functions/normalSyntax.java
@@ -0,0 +1,12 @@
+public class normalSyntax {
+  
+  public static void print(){
+    System.out.println("hlo Bro how are you");
+    return;
+  }
+  public static void main(String[] args) {
+    print();
+  }
+}
+
+// watch carefully there is void so it is basically for a empty function.we can return it simply.
