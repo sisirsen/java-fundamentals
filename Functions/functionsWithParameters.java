b479d0cf2078b14dbafdba8d68495a487b83593f
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-17 23:28:31 +0530

    functions

--- Functions/functionsWithParameters.java
+++ Functions/functionsWithParameters.java
@@ -1,7 +1,8 @@
 public class functionsWithParameters {
   
   public static int print(int a,int b){
-   return a+b;
+   int sum=a+b;
+   return sum;
   }
   public static void main(String[] args) {
     System.out.println(print(5, 7));
