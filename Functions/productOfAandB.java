a8d8edd221f3ce991f1a5083a5b668d2bbb6958c
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-18 11:46:14 +0530

    functions

---
+++ Functions/productOfAandB.java
@@ -0,0 +1,9 @@
+public class productOfAandB {
+  public static int multiply(int a, int b){
+    int product=a*b;
+    return product;
+  }
+  public static void main(String[] args) {
+    System.out.println(multiply(2, 3));
+  }
+}
