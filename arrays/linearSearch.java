2b3049250963ce440cbd92e30e1524be9f8563a7
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-21 23:42:21 +0530

    linear search

---
+++ arrays/linearSearch.java
@@ -0,0 +1,25 @@
+// public class linearSearch{
+
+//     public static int search(int arr[],int target){
+//       for (int i = 0; i < arr.length; i++) {
+       
+//         if (arr[i]==target) {
+//          return i; 
+//         }
+//     }
+//     return -1;
+//     }
+
+//   public static void main(String[] args) {
+//     int arr []={1,2,3,4,5,6,7,8,9};
+//     int target=8;
+//     int index = search(arr, target);
+//     if (index==-1) {
+//         System.out.print("num not found");
+//       }
+//       else{
+//         System.out.println("index is "+index);
+//       }
+//   }
+
+// }
