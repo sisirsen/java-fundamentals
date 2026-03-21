e62d7f8940816d3a5bc1d37d2a2e9283eb20e2ee
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-21 23:04:07 +0530

    value updation of an array

--- arrays/createArray.java
+++ arrays/createArray.java
@@ -1,11 +1,11 @@
-package arrays;
+// package arrays;
 
-public class createArray {
-  public static void main(String[] args) {
-    int array[]=new int[50]; // this line tells that only 50 values can be stored in this array and no updation of array in runtime once declared
+// public class createArray {
+//   public static void main(String[] args) {
+//     int array[]=new int[50]; // this line tells that only 50 values can be stored in this array and no updation of array in runtime once declared
 
-    int num[]={1,2,3}; //here the array is [1,2,3];
+//     int num[]={1,2,3}; //here the array is [1,2,3];
 
-    String fruits []={"apple","banana","litchi"};//we have to declare the datatype to make and run the array.
-  }
-}
+//     String fruits []={"apple","banana","litchi"};//we have to declare the datatype to make and run the array.
+//   }
+// }
---
+++ arrays/valueUpdate.java
@@ -0,0 +1,11 @@
+
+// public class valueUpdate {
+//   public static void main(String[] args) {
+//     int arr[]={1,2,3};
+
+//     arr[0]=3;
+//     for (int i = 0; i < arr.length; i++) {
+//       System.out.print(arr[i]+ " ");
+//     }
+//   }
+// }
