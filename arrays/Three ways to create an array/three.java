1bb1861d7061973fd6b91ff7aedd8b590c9fa3bb
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-25 11:38:50 +0530

    All in the same line(Declaration and memory allocation)

---
+++ arrays/Three ways to create an array/three.java
@@ -0,0 +1,15 @@
+/*
+    All in the same line(Declaration and memory allocation)
+*/
+public class three {
+  public static void main(String[] args) {
+    int [] marks={1,2,3,4,5};
+
+    marks[0]=1;
+    marks[1]=2;
+    marks[2]=3;
+    marks[3]=4;
+    marks[4]=5;
+    System.out.println(marks[4]);
+  }
+}
