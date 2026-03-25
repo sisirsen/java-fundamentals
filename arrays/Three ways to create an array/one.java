c9ab32ff7306adbf9d439c6bea356ac9a19a649d
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-25 11:32:15 +0530

    Declaration+Memoryallocation

---
+++ arrays/Three ways to create an array/one.java
@@ -0,0 +1,16 @@
+/*
+Declaration+Memoryallocation
+*/ 
+
+
+public class one {
+  public static void main(String[] args) {
+    int[] marks = new int[5];
+    marks[0]=1;
+    marks[1]=2;
+    marks[2]=3;
+    marks[3]=4;
+    marks[4]=5;
+    System.out.println(marks[4]);
+  }
+}
