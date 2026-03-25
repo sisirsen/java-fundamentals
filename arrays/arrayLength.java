bf703b497188826438638b6b58f6cf3dfc35c39e
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-25 11:49:23 +0530

    array length

---
+++ arrays/arrayLength.java
@@ -0,0 +1,27 @@
+// public class arrayLength {
+//   public static void main(String[] args) {
+//     int [] marks={1,2,3,4,5};
+
+//   System.out.println(marks.length);
+//   }
+// }
+
+
+// public class arrayLength {
+//   public static void main(String[] args) {
+//     double [] marks={1.3,2.3,3.4,4.5,5.9};
+
+//   System.out.println(marks.length);
+//   }
+// }
+
+
+// public class arrayLength {
+//   public static void main(String[] args) {
+//     String [] names={"sisir","rajkumar","shreya","shiboram"};
+
+//   System.out.println(names.length);
+//   }
+// }
+
+
