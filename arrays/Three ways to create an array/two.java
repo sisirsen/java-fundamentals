ba97fbd8e217bbb289248d2fc4c1f8d90b2a3f6b
SISIR SEN <sisirsen22122004@gmail.com>
2026-03-25 11:35:47 +0530

    Declaration   Memory allocation

---
+++ arrays/Three ways to create an array/two.java
@@ -0,0 +1,20 @@
+/*
+  Declaration
+  Memory allocation
+*/
+
+
+public class two {
+  public static void main(String[] args) {
+    int [] marks;
+    marks =new int[5];
+
+
+    marks[0]=1;
+    marks[1]=2;
+    marks[2]=3;
+    marks[3]=4;
+    marks[4]=5;
+    System.out.println(marks[4]);
+  }
+}
