702e1386c7c00b5977c2502da0c7a4a1141d7e03
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-10 22:47:59 +0530

    Scanner class to take input from user

---
+++ Scanner Class/TakeInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TakeInput {
+  public static void main(String[] args) {
+    
+    Scanner sc= new Scanner(System.in);
+
+    // System.out.println("enter a number");
+    // int num=sc.nextInt();
+    // System.out.println("the num is " + num);
+
+
+    // System.out.println("Enter a name too");
+    // String name =sc.next();
+    // System.out.println("my name is "+name);
+
+
+    // System.out.println("print a line");
+    // String line=sc.nextLine();
+    // System.out.println("The Line is : "+line);
+  }
+}
