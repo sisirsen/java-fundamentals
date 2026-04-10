88ea5c0c3af2a8af17f34424897168714118adf2
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-10 23:12:02 +0530

    Character Reader

---
+++ ReadChar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ReadChar {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+
+     System.out.println("Enter a char :");
+
+      char ch =sc.next().charAt(0);
+      System.out.println("Character is :- "+ch);
+  }
+}
