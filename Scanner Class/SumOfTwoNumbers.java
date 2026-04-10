be373efccf4bec0feb3bab21bf1a56bcb1a38782
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-10 22:54:11 +0530

    Sum of two numbers by taking input

---
+++ Scanner Class/SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+// import java.util.Scanner;
+// public class SumOfTwoNumbers {
+//   public static void main(String[] args) {
+    
+//     Scanner sc=new Scanner(System.in);
+//     System.out.println("Enter 1st no.");
+//     int a=sc.nextInt();
+//     System.out.println("Enter 2nd no.");
+//     int b=sc.nextInt();
+
+//     System.out.println("The sum of two no is : " + (a+b));
+//   }
+// }
