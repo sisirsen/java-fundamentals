d9aed8020886dfbc5b9bb7c0d6a91ed115ca50a3
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-29 10:00:00 +0530

    Ternary Operator & Switch Case in Java

---
+++ IF ELSE/Ternary Operator/problem.java
@@ -0,0 +1,9 @@
+public class problem {
+
+  public static void main(String[] args) {
+    int num=6;
+    String ans;
+    ans=(num % 2 == 0)? "even" : "odd";
+    System.out.println(ans);
+  }
+}
--- arrays/2D ARRAY/MDarray.java
+++ arrays/2D ARRAY/MDarray.java
@@ -1,20 +1,40 @@
 
 //2D Arrays
 
+// public class MDarray {
+//   public static void Mdarray (){
+
+//     int ages[][]={{1,2,3,4},{6,7,8,9}};
+
+//     System.out.println(ages[0][0]);
+//     System.out.println(ages[0][1]);
+//     System.out.println(ages[0][2]);
+//     System.out.println(ages[1][0]);
+//     System.out.println(ages[1][1]);
+//     System.out.println(ages[1][2]);
+//   }
+//   public static void main(String[] args) {
+//     Mdarray();
+//   }
+
+// }
+
 public class MDarray {
-  public static void Mdarray (){
 
-    int ages[][]={{1,2,3,4},{6,7,8,9}};
-
-    System.out.println(ages[0][0]);
-    System.out.println(ages[0][1]);
-    System.out.println(ages[0][2]);
-    System.out.println(ages[1][0]);
-    System.out.println(ages[1][1]);
-    System.out.println(ages[1][2]);
-  }
+  public static void maxval(){
+    int arr[]={1,2,3,4,5,3};
+    int target=3;
+    int ans= -1;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i]==target) {
+        ans=i;
+        break;
+      }
+    }
+    System.out.println(ans);
+    }
+  
   public static void main(String[] args) {
-    Mdarray();
+    maxval();
   }
-
 }
