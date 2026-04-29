dc0fe472b20ef6f0941c47944fcfe8350452f0ec
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-29 12:12:38 +0530

    2D Arrays In Java

---
+++ arrays/2D ARRAY/MDarray.java
@@ -0,0 +1,20 @@
+
+//2D Arrays
+
+public class MDarray {
+  public static void Mdarray (){
+
+    int ages[][]={{1,2,3,4},{6,7,8,9}};
+
+    System.out.println(ages[0][0]);
+    System.out.println(ages[0][1]);
+    System.out.println(ages[0][2]);
+    System.out.println(ages[1][0]);
+    System.out.println(ages[1][1]);
+    System.out.println(ages[1][2]);
+  }
+  public static void main(String[] args) {
+    Mdarray();
+  }
+
+}
