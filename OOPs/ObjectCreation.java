ea618aca28bb050ee100191a8d1bae20e73057bb
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-30 10:00:00 +0530

    OOPs in Jva How to create Object

---
+++ OOPs/ObjectCreation.java
@@ -0,0 +1,26 @@
+// public class ObjectCreation {
+  
+//     String schoolName= "Mathura Vidyalaya";
+//     int Rollno=01;
+
+//     public static void main(String[] args) {
+
+//     ObjectCreation sisir = new ObjectCreation();
+//     System.out.println(sisir.Rollno);
+//     System.out.println(sisir.schoolName);
+//   }
+// }
+
+class ObjectCreation {
+
+  int electricBill=2100;
+  String name="ramu cacha";
+
+  public static void main(String[] args) {
+    
+    ObjectCreation customer= new ObjectCreation();
+
+    System.out.println(customer.electricBill);
+    System.out.println(customer.name);
+  }
+}
