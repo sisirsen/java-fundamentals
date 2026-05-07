c03b26b42d1e3f8aae46ff26d9ed7dd67916bf14
SISIR SEN <sisirsen22122004@gmail.com>
2026-05-07 22:43:46 +0530

    Java Math Built In  methods

---
+++ Math Built Ins/problem.java
@@ -0,0 +1,9 @@
+public class problem {
+  public static void main(String[] args) {
+    
+    System.out.println(Math.sqrt(49f));
+    System.out.println(Math.floor(5.7));
+    System.out.println(Math.ceil(5.5));
+   
+  }
+}
